e6a0d7465cc4afa9dc1136bfa6b3db25c1d3922c
h_mochizuki <bda5114128@gmail.com>
2018-10-24 23:51:50 +0900

    add_styles_CalculatorComponents

--- src/components/ClearBtn.js
+++ src/components/ClearBtn.js
@@ -1,7 +1,15 @@
 import React, { PropTypes } from 'react';
 
 const ClearBtn = ({ onClick }) => (
-  <button onClick={ onClick }>C</button>
+  <button onClick={ onClick } style={{
+    color:'rgb(50, 150, 250)',
+    backgroundColor: 'rgba(3, 169, 244, 0.08)',
+    borderRadius: '33%',
+    width: '30px',
+    height: '30px',
+    margin: '1px',
+    border: 'outset rgb(50,150,250)',
+  }}>C</button>
 )
 
 ClearBtn.propTypes = {
